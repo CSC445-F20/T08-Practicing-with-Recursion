ced6185633c968540794c9d9faa898b2b8d0c8e9
shepherdp <88725439+shepherdp@users.noreply.github.com>
2022-10-19 23:28:17 -0400

    Create t08.py

---
+++ t08.py
@@ -0,0 +1,30 @@
+# CSC 445 - Fall 2022
+# Code for T08: Practicing with Recursion
+# Author: Patrick Shepherd
+
+# This file gives you stubs for five recursive functions you must complete.
+
+import os
+
+# Function 1: Determine if an element is in a list.
+def findme(item, thelist):
+    pass
+  
+# Function 2: Determine the number of steps it takes to solve Tower of Hanoi with n rings.
+def tower(n):
+    pass
+  
+# Function 3: Determine if a string is a palindrome.
+def ispalindrome(thestring):
+    pass
+  
+# Function 4: Pretty-print a directory tree on your computer
+def printtree(f):
+    pass
+  
+# Function 5: Implement MergeSort, but with a 3-way split instead of 2 way.
+def merge(lista, listb, listc):
+    pass
+  
+def mergesort(thelist):
+    pass
